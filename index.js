4b2aa882bf24e12de40935d1d5610e89fa373a55
James Lu <jdl8181@nyu.edu>
2022-06-14 10:48:38 -0400

    created index.js as well as other files that come with using Heroku

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const path = require('path');
 const express = require('express');
+const { Client } = require('@googlemaps/google-maps-services-js');
 const app = express();
+const client = new Client({}); 
+require('dotenv').config({ path: `${__dirname}/../git-one/.env`})
+
+client
+    .geocode({
+        params:{
+            key: process.env.API_KEY
+        },
+    })
 app.use(express.static(path.join(__dirname, '..', 'git-one')))
 
 app.get('/', (req, res) => {
